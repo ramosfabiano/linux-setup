519991c27df5fbb9225d49e882cc47d712486eb4
ramosfabiano <ramos_fabiano@yahoo.com.br>
2026-02-06 18:35:47 -0300

    Switch back to vscode.

--- fedora-43-setup.sh
+++ fedora-43-setup.sh
@@ -241,8 +241,8 @@ auto() {
     msg 'Installing veracrypt'
     install_veracrypt
     msg 'Installing coding tools'
-    install_cursor
-    #install_vscode
+    #install_cursor
+    install_vscode
     msg 'Disabling smart card'
     disable_smart_card
     msg 'Installing qemu'
